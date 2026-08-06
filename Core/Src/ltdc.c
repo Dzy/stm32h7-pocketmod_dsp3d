dbaf5bb668952027f42f343643bfe77ea718a9ca
Dzy <3352717+Dzy@users.noreply.github.com>
2026-08-06 19:04:24 +0300

    Match packaged LTDC mode table

--- Core/Src/ltdc.c
+++ Core/Src/ltdc.c
@@ -39,12 +39,13 @@ const LTDCSYNC_t LTDCSYNC[] = {
 };
 
 #define LTDC_MODE_COUNT (sizeof(LTDCSYNC) / sizeof(LTDCSYNC[0]))
-_Static_assert(LTDC_VID_FORMAT < LTDC_MODE_COUNT, "LTDC_VID_FORMAT is outside LTDCSYNC[]");
+_Static_assert(LTDC_VID_FORMAT < LTDC_MODE_COUNT, "LTDC_VID_FORMAT is outside LTDCSYNC[]");    
 
 /* USER CODE END 0 */
 
 LTDC_HandleTypeDef hltdc;
 
+/* LTDC init function */
 void MX_LTDC_Init(void)
 {
   LTDC_LayerCfgTypeDef pLayerCfg;
@@ -59,8 +60,8 @@ void MX_LTDC_Init(void)
   hltdc.Init.VerticalSync       = (LTDCSYNC[LTDC_VID_FORMAT].vsh - 1);
   hltdc.Init.AccumulatedHBP     = (LTDCSYNC[LTDC_VID_FORMAT].hsw + LTDCSYNC[LTDC_VID_FORMAT].hbp - 1);
   hltdc.Init.AccumulatedVBP     = (LTDCSYNC[LTDC_VID_FORMAT].vsh + LTDCSYNC[LTDC_VID_FORMAT].vbp - 1);
-  hltdc.Init.AccumulatedActiveW = (LTDCSYNC[LTDC_VID_FORMAT].hsw + LTDCSYNC[LTDC_VID_FORMAT].ahw + LTDCSYNC[LTDC_VID_FORMAT].hbp - 1);
-  hltdc.Init.AccumulatedActiveH = (LTDCSYNC[LTDC_VID_FORMAT].vsh + LTDCSYNC[LTDC_VID_FORMAT].avh + LTDCSYNC[LTDC_VID_FORMAT].vbp - 1);
+  hltdc.Init.AccumulatedActiveW = (LTDCSYNC[LTDC_VID_FORMAT].hsw + LTDCSYNC[LTDC_VID_FORMAT].ahw + LTDCSYNC[LTDC_VID_FORMAT].hbp  - 1);
+  hltdc.Init.AccumulatedActiveH = (LTDCSYNC[LTDC_VID_FORMAT].vsh + LTDCSYNC[LTDC_VID_FORMAT].avh + LTDCSYNC[LTDC_VID_FORMAT].vbp  - 1);
   hltdc.Init.TotalWidth         = (LTDCSYNC[LTDC_VID_FORMAT].hsw + LTDCSYNC[LTDC_VID_FORMAT].ahw + LTDCSYNC[LTDC_VID_FORMAT].hbp + LTDCSYNC[LTDC_VID_FORMAT].hfp - 1);
   hltdc.Init.TotalHeigh         = (LTDCSYNC[LTDC_VID_FORMAT].vsh + LTDCSYNC[LTDC_VID_FORMAT].avh + LTDCSYNC[LTDC_VID_FORMAT].vbp + LTDCSYNC[LTDC_VID_FORMAT].vfp - 1);
 
@@ -73,7 +74,7 @@ void MX_LTDC_Init(void)
   pLayerCfg.WindowX1 = LTDCSYNC[LTDC_VID_FORMAT].ahw;
   pLayerCfg.WindowY0 = 0;
   pLayerCfg.WindowY1 = LTDCSYNC[LTDC_VID_FORMAT].avh;
-  pLayerCfg.PixelFormat = LTDC_PIXEL_FORMAT_L8;
+  pLayerCfg.PixelFormat = LTDC_PIXEL_FORMAT_L8; //RGB565;
   pLayerCfg.Alpha = 255;
   pLayerCfg.Alpha0 = 0;
   pLayerCfg.BlendingFactor1 = LTDC_BLENDING_FACTOR1_PAxCA;
@@ -85,14 +86,21 @@ void MX_LTDC_Init(void)
   pLayerCfg.Backcolor.Green = 255;
   pLayerCfg.Backcolor.Red = 255;
   HAL_LTDC_ConfigLayer(&hltdc, &pLayerCfg, 0);
+
 }
 
 void HAL_LTDC_MspInit(LTDC_HandleTypeDef* ltdcHandle)
 {
+
   GPIO_InitTypeDef GPIO_InitStruct;
   if(ltdcHandle->Instance==LTDC)
   {
+  /* USER CODE BEGIN LTDC_MspInit 0 */
+
+  /* USER CODE END LTDC_MspInit 0 */
+    /* LTDC clock enable */
     __HAL_RCC_LTDC_CLK_ENABLE();
+
     __HAL_RCC_GPIOI_CLK_ENABLE();
     __HAL_RCC_GPIOF_CLK_ENABLE();
     __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -101,7 +109,32 @@ void HAL_LTDC_MspInit(LTDC_HandleTypeDef* ltdcHandle)
     __HAL_RCC_GPIOB_CLK_ENABLE();
     __HAL_RCC_GPIOG_CLK_ENABLE();
     __HAL_RCC_GPIOD_CLK_ENABLE();
+    /**LTDC GPIO Configuration
+    
+    PF10    ------> LTDC_DE
+    PA4     ------> LTDC_VSYNC
+    PG7     ------> LTDC_CLK
+    PC6     ------> LTDC_HSYNC
+    
+    PB0     ------> LTDC_R3
+    PA5     ------> LTDC_R4
+    PC0     ------> LTDC_R5
+    PB1     ------> LTDC_R6
+    PG6     ------> LTDC_R7
 
+    PA6     ------> LTDC_G2
+    PC9     ------> LTDC_G3
+    PB10     ------> LTDC_G4
+    PH4     ------> LTDC_G5
+    PI11    ------> LTDC_G6
+    PD3     ------> LTDC_G7
+
+    PA8     ------> LTDC_B3
+    PG12     ------> LTDC_B4
+    PA3     ------> LTDC_B5
+    PB8     ------> LTDC_B6
+    PB9     ------> LTDC_B7
+    */
     GPIO_InitStruct.Pin = GPIO_PIN_11;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
@@ -110,77 +143,66 @@ void HAL_LTDC_MspInit(LTDC_HandleTypeDef* ltdcHandle)
     HAL_GPIO_Init(GPIOI, &GPIO_InitStruct);
 
     GPIO_InitStruct.Pin = GPIO_PIN_4;
+    //GPIO_InitStruct.Alternate = GPIO_AF9_LTDC;
     HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+    //GPIO_InitStruct.Alternate = GPIO_AF9_LTDC;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = GPIO_PIN_12;
+    //GPIO_InitStruct.Alternate = GPIO_AF9_LTDC;
     HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = GPIO_PIN_10;
     GPIO_InitStruct.Alternate = GPIO_AF14_LTDC;
     HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_6;
+    //GPIO_InitStruct.Alternate = GPIO_AF14_LTDC;
     HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6;
+    //GPIO_InitStruct.Alternate = GPIO_AF14_LTDC;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_8|GPIO_PIN_9;
+    //GPIO_InitStruct.Alternate = GPIO_AF14_LTDC;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = GPIO_PIN_6;
+    //GPIO_InitStruct.Alternate = GPIO_AF14_LTDC;
     HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = GPIO_PIN_7;
+    //GPIO_InitStruct.Alternate = GPIO_AF14_LTDC;
     HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = GPIO_PIN_3;
+    //GPIO_InitStruct.Alternate = GPIO_AF14_LTDC;
     HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = GPIO_PIN_9;
     GPIO_InitStruct.Alternate = GPIO_AF10_LTDC;
     HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
     GPIO_InitStruct.Pin = GPIO_PIN_8;
     GPIO_InitStruct.Alternate = GPIO_AF13_LTDC;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+    /* LTDC interrupt Init */
     HAL_NVIC_SetPriority(LTDC_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(LTDC_IRQn);
     HAL_NVIC_SetPriority(LTDC_ER_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(LTDC_ER_IRQn);
+  /* USER CODE BEGIN LTDC_MspInit 1 */
+
+  /* USER CODE END LTDC_MspInit 1 */
   }
 }
 
-extern void w_reg(uint16_t reg, uint8_t val);
-extern void s_reg(uint16_t reg, uint8_t val);
-extern void c_reg(uint16_t reg, uint8_t val);
+/* USER CODE BEGIN 1 */
 
-#define TDA_REG_IF2_HB0       0x1040U
-#define TDA_REG_DIP_IF_FLAGS  0x110FU
-#define TDA_DIP_IF_FLAGS_IF2  (1U << 2)
-
-void tda_apply_avi_infoframe(void)
-{
-  const LTDCSYNC_t *mode = &LTDCSYNC[LTDC_VID_FORMAT];
-  uint8_t frame[17] = {0};
-  uint8_t checksum = 0U;
-  uint32_t i;
-
-  c_reg(TDA_REG_DIP_IF_FLAGS, TDA_DIP_IF_FLAGS_IF2);
-  if (mode->hdmi_vic == 0U)
-    return;
-
-  frame[0] = 0x82U;
-  frame[1] = 0x02U;
-  frame[2] = 13U;
-  frame[3] = 0U;
-  frame[4] = 0U;
-  frame[5] = (uint8_t)((mode->hdmi_aspect & 0x03U) << 4);
-  frame[6] = (uint8_t)(2U << 2);
-  frame[7] = (uint8_t)(mode->hdmi_vic & 0x7FU);
-  frame[8] = 0U;
-
-  for (i = 0U; i < sizeof(frame); ++i)
-    checksum = (uint8_t)(checksum + frame[i]);
-  frame[3] = (uint8_t)(0U - checksum);
-
-  for (i = 0U; i < sizeof(frame); ++i)
-    w_reg((uint16_t)(TDA_REG_IF2_HB0 + i), frame[i]);
-
-  s_reg(TDA_REG_DIP_IF_FLAGS, TDA_DIP_IF_FLAGS_IF2);
-}
+/* USER CODE END 1 */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
